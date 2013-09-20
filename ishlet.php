35072876f7081117727a50adefebcaa3633d02e7
Joe McGill <joemcgill@gmail.com>
2013-09-19 23:18:07 -0500

    ADDED: created landing page for installing the bookmarklet

---
+++ ishlet.php
@@ -0,0 +1,53 @@
+<?php
+
+$js = file_get_contents("ishlet.js");
+$js_html = htmlentities($js);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>'+d.title+'-ish.</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width" />
+	<style type="text/css">
+
+		body {
+			padding-top: 3em;
+			background-color: #fafbfd;
+			font-family: helvetica, arial, sans-serif;
+			text-align: center;
+		}
+
+		.container {
+			max-width: 25em;
+			margin: 2em auto;
+			padding: 4em 0;
+		}
+
+		.btn {
+			display: block;
+			margin-bottom: .5em;
+			padding:.5em;
+			background-color: #ccc;
+			-webkit-border-radius: 8px;
+			-moz-border-radius: 8px;
+			border-radius: 8px;
+			font-weight: bold;
+			color: #333;
+			text-decoration: none;
+			text-transform: uppercase;
+		}
+
+	</style>
+</head>
+<body>
+	<h1>ishlet.</h1>
+	<h2>A bookmarklet version of the viewport resizer by Brad Frost</h2>
+
+	<div class="container">
+		<a class='btn' href="<?php echo($js_html); ?>">ishlet.</a>
+		<small>Drag link to the bookmarks bar to install</small>
+	</div>
+
+</body>
