2cb27ecf42fe711322d6b40d2b02cbb552891157
Vasilis van Gemert <vvangemert@raapstelen.fritz.box>
2013-10-27 08:34:42 +0100

    Added a title parameter
    Added an optional title parameter which shows the title of the iframed
    page if the bookmarklet is used.

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ish. yet another viewport resizer.</title>
+<?php //Get URL Parameter
+	$src = (empty($_GET['url'])) ? 'http://bradfrostweb.com/blog/post/ish' : addslashes(filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL));
+	// Get an optional Title Parameter. Handy for more than one instance of ish during presentations
+	$title = (empty($_GET['title'])) ? 'ish. yet another viewport resizer.' : filter_input(INPUT_GET, 'title');
+?>
+
     <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" href="styles.css" media="all" />
 </head>
 <body>
-<?php //Get URL Parameter
-	$src = (empty($_GET['url'])) ? 'http://bradfrostweb.com/blog/post/ish' : addslashes(filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL));
-?>
 <!--iFrame-->
 <div id="vp-wrap"><iframe id="viewport" src="<?php echo $src; ?>"></iframe></div>
 <!--end iFrame-->
