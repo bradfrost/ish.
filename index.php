d8e130d604a6062ca49af9658e91e9336b039c34
Vasilis van Gemert <vvangemert@raapstelen.fritz.box>
2013-10-27 09:31:06 +0100

    Replaced old ish funciotnality with patterlab functionality
    This is basically a patternlab version of ish without the patternlab,
    but with the bookmarklet

--- index.php
+++ index.php
@@ -10,48 +10,64 @@
     <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width" />
-    <link rel="stylesheet" href="styles.css" media="all" />
+    <link rel="stylesheet" href="styleguide/css/styleguide.css" media="all" />
+    <script src="styleguide/js/jquery.js"></script>
 </head>
-<body>
-<!--iFrame-->
-<div id="vp-wrap"><iframe id="viewport" src="<?php echo $src; ?>"></iframe></div>
-<!--end iFrame-->
-<ul class="nav size" id="nav">
-	<li><a href="#" id="size-toggle" class="active">Size</a></li>
-	<li><a href="#" id="size-s">S</a></li>
-	<li><a href="#" id="size-m">M</a></li>
-	<li><a href="#" id="size-l">L</a></li>
-	<li><a href="#" id="size-xl">XL</a></li>
-	<li><a href="#" id="size-full">100%</a></li>
-	<li><a href="#" id="size-random">Random</a></li>
-	<li><a href="#" id="size-disco">Disco</a></li>
-	<li>
-		<form method="post" action="#" id="size-enter" >
-			<input id="size-num" type="number" pattern="[0-9]*" step="10" min="100" placeholder="Enter Size" />
-		</form>
-	</li>
-</ul>
-
-<div class="nav url-change">
-	<?php 
+<body class="sg-nav-wrapper">
+<!--Style Guide Header-->
+<header class="sg-header" role="banner">
+	<a href="#sg-nav-container" class="sg-nav-toggle">Menu</a>
+	<div class="sg-nav-container" id="sg-nav-container" style="min-height: 33px">
+		<!--Begin Pattern Navigation-->
+<?php 
 //Get the correct URL for this domain. Is there really no easier way to do this?
 $protocol = (empty($_SERVER['HTTPS'])) ? 'http' : 'https';
 $bookmarkletURL = $protocol."://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]";
 ?>
-	<a href="javascript:location.href='<?php echo $bookmarkletURL; ?>?title='+document.title+'&url='+window.location.href" title="The ish bookmarklet">ish</a>
-</div>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
-<script type="text/javascript" src="js/init.js "></script>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-2687475-3']);
-  _gaq.push(['_trackPageview']);
+		<a href="javascript:location.href='<?php echo $bookmarkletURL; ?>?title='+document.title+'&url='+window.location.href">ish</a>
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+		<!--End Pattern Navigation-->
+		
+		<!--Begin Controls-->
+		<div class="sg-controls" id="sg-controls">
+				<div class="sg-control-content">
+					<ul class="sg-control">
+						<li class="sg-size">
+							<div class="sg-current-size"><form id="sg-form">Size <input type="text" class="sg-input sg-size-px" value="320">px / <input type="text" class="sg-input sg-size-em" value="20">em</form></div>
+							<ul class="sg-acc-panel sg-size-options">
+								<li class="sg-quarter"><a href="#" id="sg-size-s">S</a></li> 
+								<li class="sg-quarter"><a href="#" id="sg-size-m">M</a></li>
+								<li class="sg-quarter"><a href="#" id="sg-size-l">L</a></li>
+								<li class="sg-quarter"><a href="#" id="sg-size-xl">XL</a></li>
+								<li class="sg-half"><a href="#" id="sg-size-full">Full</a></li>
+								<li class="sg-half"><a href="#" id="sg-size-random">Random</a></li>
+								<li class="sg-half"><a href="#" class="mode-link" id="sg-size-disco">Disco</a></li>
+								<li class="sg-half"><a href="#" class="mode-link" id="sg-size-hay">Hay!</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+		</div>
+		<!--End Controls-->
+	</div>
+</header>
+<!--End Style Guide Header-->
+
+
+<!-- Iframe -->
+<div id="sg-vp-wrap">
+	<div id="sg-cover"></div>
+	<div id="sg-gen-container">
+		<iframe id="sg-viewport" src="<?php echo $src; ?>" style="background: #fff;"></iframe>
+		<div id="sg-rightpull-container">
+			<div id="sg-rightpull"></div>
+		</div>
+	</div>
+</div>
+<!--end iFrame-->
+
+<script src="styleguide/js/data-saver.js"></script>
+<script src="styleguide/js/styleguide.js "></script>
+<script src="styleguide/js/annotations-viewer.js "></script>
 </body>
 </html>
